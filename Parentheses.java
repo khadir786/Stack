9009c374764cab2236cf4f13f3cd5defd6373824
Khadir Abdul <khadir786.ka@googlemail.com>
2023-07-27 21:17:54 +0100

    Added parentheses validation

---
+++ Parentheses.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()){
+            try {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            }
+            else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            }
+            
+            else return false;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+
+
+
+public static void main(String[] args) throws Exception {
+        Parentheses pa = new Parentheses();
+        String chars = "]";
+
+        boolean result = pa.isValid(chars);
+        System.out.println((result));
+    }
+}
